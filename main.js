3d8bd859f2803d0a2ba63fe67993c7769d96aa2e
Ganesh <krganesh1611@gmail.com>
2021-08-08 17:45:04 +0530

    Organize function implemented and added gitignore till now it created main directory as "organizedFiles"

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let fs = require("fs");
+let path = require("path");
 let inputArr = process.argv.slice(2);
 // input always starts from 2nd word. for ex:- node main.js Hello how are you.
 // Output will be [ 'Hello', 'how', 'are', 'you.' ]
@@ -5,17 +7,20 @@ let inputArr = process.argv.slice(2);
 /* Command to implements
 * node main.js tree "directoryPath"
 * node main.js organise "directoryPath"
-* node main.js tree
+* node main.js help
 */
 let command = inputArr[0];
 
 switch(command) {
     case "tree":
+        treeFn(inputArr[1]);
         break;
     case "organize": 
+        organizeFn(inputArr[1]);
         break;
 
     case "help":
+        helpFn();
         break;
 
     default:
@@ -28,10 +33,42 @@ function treeFn(dirPath){
 }
 
 function organizeFn(dirPath){
-    console.log("Tree command implement for ", dirPath);
+    // console.log("Organize command implement for ", dirPath);
+    
+    // check if path was not provided by the user
+    if(dirPath == undefined){
+        console.log(`path not provided error...
+                also provide the path
+        `);
+        return;
+    } else{
+        let doesExist = fs.existsSync(dirPath);
+        // check if the path provided by the user is available or not
+        if(doesExist){
+
+            // create path 
+            let destPath = path.join(dirPath, "organisedFiles");
+            // if dir does not exist then create the directory
+            if(fs.existsSync(destPath) == false){
+                fs.mkdirSync(destPath);     // creation of folder at that path
+            } 
+
+        } else{
+            console.log(`path not found error...
+                    enter the valid path
+            `);
+            return;
+        }
+    }
+
 }
+
 
 function helpFn(dirPath){
-    console.log("Tree command implement for ", dirPath);
+    console.log(`
+    List of All the commands:
+            node main.js tree "directoryPath"
+            node main.js organise "directoryPath"
+            node main.js help
+    `);
 }
-
